bfacdaa8a8da5ad28149163453c15ccdad8ef47f
Avinash Kumar A <90566941+avinashkallingal@users.noreply.github.com>
2024-10-03 05:34:50 +0530

    feat:added post edit

--- src/modules/post/postController.ts
+++ src/modules/post/postController.ts
@@ -17,6 +17,7 @@ interface RabbitMQResponse<T> {
     success: boolean;
     message: string;
     data?: T;
+    like?:boolean;
 }
 
 interface data {
@@ -36,7 +37,7 @@ export const postController = {
 
     addPost: async (req: any, res: Response) => {
         try {
-            console.log('addPost function called')
+            console.log('addPost function called+++++++++++')
             // console.log(req.files, '++++++', req.body)
             const data: data = req.body;
             // const images = req.files as { [fieldname: string]: Express.Multer.File[] } | Express.Multer.File[];
@@ -74,6 +75,56 @@ export const postController = {
             console.log("+++++++++ sending to post_queu",images)
             const response = await postRabbitMqClient.produce(
                 { userId, summary, title, genre, images ,pdf,tags}, operation
+            )
+            console.log('3')
+            console.log(response, '-------------------post added to database');
+            return res.status(200).json(response);
+        } catch (error) {
+            console.log('4')
+            return res.status(500).json({ success: false, message: 'Error occurred while creating new post' });
+        }
+    },
+    editPost: async (req: any, res: Response) => {
+        try {
+            console.log('editPost function called++++++++++++++')
+         
+            const data: any = req.body;
+           
+            const images = req.files["photoFile"]
+            const pdf=req.files["pdfFile"]
+
+            // if (!data.userId) {
+            //     return res.status(400).json({ success: false, message: 'UserId is missing' });
+            // }
+            // console.log('1');
+
+            // if (images) {
+            //     let filesArray: Express.Multer.File[] = [];
+            //     if (Array.isArray(images)) {
+            //         filesArray = images;
+            //     } else {
+            //         filesArray = Object.values(images).flat();
+            //     }
+
+            //     for (let file of filesArray) {
+            //         if (!validImageMimeTypes.includes(file.mimetype)) {
+            //             return res.status(400).json({ error: "Only image files are allowed" });
+            //         }
+            //     }
+            // }
+
+            const userId = data.userId;
+            const summary = data.summary;
+            const title = data.title;
+            const tags=data.tags
+            const genre=data.genre
+            const postId=data.postId
+            
+
+            const operation = 'edit-post';
+            console.log("+++++++++ sending to post_queu",images)
+            const response = await postRabbitMqClient.produce(
+                { userId, summary, title, genre, images ,pdf,tags,postId}, operation
             )
             console.log('3')
             console.log(response, '-------------------post added to database');
@@ -140,6 +191,53 @@ export const postController = {
         }
         catch(e){
             console.log("error in fetching pdf url in pdfUrlFetch function ",e)
+        }
+    },
+
+    likePost:async (req: Request, res: Response) => {
+
+        try{
+            const data: any = req.body;
+            const operation = "like-post"
+            const result=await postRabbitMqClient.produce(data, operation) as RabbitMQResponse<Post[]>;
+            if(result.success){
+                if(result.like){
+                    return res.status(200).json({success:true,message:"Liked",like:true})
+                  }else{
+                    return res.status(200).json({success:true,message:"Unliked",like:false})
+                  }
+               
+            }else{
+                return res.status(500).json({success:false,message:"Internal server error"})
+            }
+            
+
+        }catch (error) {
+            console.error('Error in likePost:', error);
+            res.status(500).json({ success: false, message: 'Internal server error' });
+        }
+    },
+
+    deletePost:async (req: Request, res: Response) => {
+
+        try{
+            const data: any = req.body;
+            const operation = "delete-post"
+            const result=await postRabbitMqClient.produce(data, operation) as RabbitMQResponse<Post[]>;
+            if(result.success){
+               
+                    return res.status(200).json({success:true,message:"deleted"})
+                 
+                
+               
+            }else{
+                return res.status(500).json({success:false,message:"Internal server error"})
+            }
+            
+
+        }catch (error) {
+            console.error('Error in likePost:', error);
+            res.status(500).json({ success: false, message: 'Internal server error' });
         }
     }
 
--- src/modules/post/postRoutes.ts
+++ src/modules/post/postRoutes.ts
@@ -10,9 +10,19 @@ postRoutes.post('/addPost', upload.fields([
     { name: 'pdfFile', maxCount: 1 }     // Handling a single PDF file
   ]),postController.addPost)
 
+  postRoutes.post('/editPost', upload.fields([
+    { name: 'photoFile', maxCount: 5 },  // Handling an array of images 
+    { name: 'pdfFile', maxCount: 1 }     // Handling a single PDF file
+  ]),postController.editPost)
+
 // postRoutes.post('/addPost', authencticateToken, upload.array('files'), postController.addPost);
 postRoutes.get('/getAllPosts', postController.getAllPosts)
 postRoutes.post('/pdfUrlFetch',postController.pdfUrlFetch)
+postRoutes.post('/likePost',postController.likePost)
+postRoutes.post('/deletePost',postController.deletePost)
+
+
+
 
 
 export { postRoutes };
